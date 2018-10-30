bd7d9f7ae19d1bada3de7dc29e397e0075bbd04e
JojiAndela <42158522+JojiAndela@users.noreply.github.com>
2018-10-30 06:56:52 +0100

    (feat): user can view a ride
    - view a ride mock
    
    [(Finishes): #161090210]

--- src/containers/SingleRide/index.js
+++ src/containers/SingleRide/index.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-// import classes from './style.scss';
+import classes from './style.scss';
 
 class SingleRide extends Component {
   render() {
     return(
-      <div>
-        Got here
+      <div className={classes.SingleRide}>
+        <div className={classes.SingleCard}>
+
+            <h2> ride-lasgidi</h2>
+            <h4>Departure Location:
+                <small> maryland</small>
+            </h4>
+            <h4>Destination:
+                <small> ikeja</small>
+            </h4>
+            <h4>Departure Time:
+                <small> 11:22 PM</small>
+            </h4>
+            <h4>Departure Time:
+                <small> Mon Aug 06 2018</small>
+            </h4>
+            <h4>Avalaible slot:
+                <small> 1</small>
+            </h4>
+            <h4>Vehicle:
+                <small> sienna v2</small>
+            </h4>
+            <h4>Ride Status:
+              <small> completed</small> </h4>
+        </div>
+        <div className={classes.SingleCard}>
+          <h2>Your Request is sent!</h2>
+        </div>
       </div>
     )
   }
