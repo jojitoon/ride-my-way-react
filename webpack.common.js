27512f61f120679bd77edd13a7ab9f768858c306
JojiAndela <42158522+JojiAndela@users.noreply.github.com>
2018-10-31 07:00:34 +0100

    (chore): setup heroku and travis
    - setup server
    - setup travis
    
    [(Finishes): #161611620]

---
+++ webpack.common.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: '[id].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      // {
+      //   enforce: 'pre',
+      //   exclude: /node_modules/,
+      //   test: /\.jsx?$/,
+      //   use: 'eslint-loader'
+      // },
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              modules: true,
+              localIdentName: '[name]__[local]__[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  browsers: ['>1%', 'last 2 versions']
+                })
+              ]
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|gif|png)$/i,
+        use:[
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.css', '.scss'],
+    alias: {
+      Assets: path.resolve(__dirname, 'src/assets/'),
+      Components: path.resolve(__dirname, 'src/components/'),
+      Containers: path.resolve(__dirname, 'src/containers/'),
+      Hoc: path.resolve(__dirname, 'src/hoc/'),
+      Utility: path.resolve(__dirname, 'src/utility/'),
+      Store: path.resolve(__dirname, 'src/store/')
+    }
+  },
+  plugins:     [
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
